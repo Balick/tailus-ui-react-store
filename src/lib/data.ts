723d878abb2a0560d9b493e7cb61a1b3bbf523f2
Théo Balick <balickmethens@gmail.com>
2024-08-03 11:47:59 +0100

    [feat] Create data recovery functions

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -4,16 +4,34 @@ export async function fetchProducts() {
   try {
     // We artificially delay a response for demo purposes.
     // Don't do this in production :)
-    console.log("Fetching revenue data...");
-    await new Promise((resolve) => setTimeout(resolve, 5000));
+    console.log("Fetching products data...");
+    await new Promise((resolve) => setTimeout(resolve, 1000));
 
     const data = products;
 
-    console.log("Data fetch completed after 2 seconds.");
+    console.log("Data fetch completed after 1 second.");
 
     return data;
   } catch (error) {
     console.error("Database Error:", error);
     throw new Error("Failed to fetch products data.");
+  }
+}
+
+export async function fetchProduct(productName: string) {
+  try {
+    // We artificially delay a response for demo purposes.
+    // Don't do this in production :)
+    console.log("Fetching product data...");
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    const data = products.find(({ name }) => name === productName);
+
+    console.log("Data fetch completed after 1 second.");
+
+    return data;
+  } catch (error) {
+    console.error("Failed to fetch product data.:", error);
+    // throw new Error("Failed to fetch product data.");
   }
 }
