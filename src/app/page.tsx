482603a0e0fd41fb1f7b680152083facb4c475ce
Théo Balick <balickmethens@gmail.com>
2024-08-03 11:52:04 +0100

    [feat] Set up rescue components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,15 @@
-import { ProductCard } from "@/components/ProductCard";
-import React, { Fragment, Suspense } from "react";
-import { fetchProducts } from "@lib/data";
-import {CardsSkeleton, ProductsSkeleton} from "@/components/skeletons";
+import React, { Suspense } from "react";
+import { ProductListSkeleton } from "@/components/skeleton";
+import ProductList from "@/components/ProductList";
 
-const ProductList = async () => {
-  const products = await fetchProducts();
-
-  return (
-    <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-12">
-      {products.map((product, idx) => (
-        <Fragment key={idx}>
-          <ProductCard {...product} />
-        </Fragment>
-      ))}
-    </div>
-  );
-};
-
-const Home = () => {
+export default function Home() {
   return (
     <section className="pt-24 lg:pt-32 pb-40">
       <div className="max-w-7xl mx-auto px-6">
-        <Suspense fallback={<CardsSkeleton />}>
+        <Suspense fallback={<ProductListSkeleton />}>
           <ProductList />
         </Suspense>
       </div>
     </section>
   );
-};
-
-export default Home;
+}
--- src/app/product/[name]/page.tsx
+++ src/app/product/[name]/page.tsx
@@ -1,116 +1,28 @@
-import { Rating, RatingComparison } from "@/components/Rating";
-import { SizeRadioGroup } from "@/components/SizeRadioGroup";
-import { Text, Title, List } from "@tailus-ui/typography";
-import Button from "@tailus-ui/Button";
-import { products } from "@/constants";
-import { ProductCard } from "@/components/ProductCard";
-import { Fragment } from "react";
-import Image from "next/image";
-import { notFound } from "next/navigation";
+import { Title } from "@tailus-ui/typography";
+import React, { Fragment, Suspense } from "react";
+import { ProductListSkeleton, ProductSkeleton } from "@/components/skeleton";
+import Product from "@/components/Product";
+import SimilarProducts from "@/components/SimilarProducts";
 
 export default function Page({ params }: { params: { name: string } }) {
-  const product = products.find(
-    ({ name }) => name === decodeURIComponent(params.name),
-  );
-
-  if (!product) notFound();
-
   return (
     <>
       <section className="pt-24 sm:pt-32">
         <div className="mx-auto max-w-7xl px-6">
-          <div className="grid gap-12 lg:grid-cols-2">
-            <div className="-mx-6 flex snap-x snap-proximity gap-4 overflow-x-auto px-6 lg:grid lg:grid-cols-2 lg:gap-6 lg:overflow-visible">
-              <div className="relative w-max min-w-80 snap-center overflow-hidden rounded-[--card-radius] lg:col-span-2 lg:w-fit lg:min-w-full lg:max-w-full">
-                <Image
-                  src={product.image}
-                  alt={`image of ${product.name}`}
-                  width={500}
-                  height={500}
-                  priority
-                  className="size-full rounded-[--card-radius] border object-cover group-hover:scale-105"
-                />
-              </div>
-              <div className="relative w-max min-w-80 snap-center overflow-hidden rounded-[--card-radius] lg:w-fit lg:min-w-full lg:max-w-full">
-                <Image
-                  src={product.image}
-                  alt={`image of ${product.name}`}
-                  width={500}
-                  height={500}
-                  priority
-                  className="size-full rounded-[--card-radius] border object-cover group-hover:scale-105"
-                />
-              </div>
-              <div className="relative w-max min-w-80 snap-center overflow-hidden rounded-[--card-radius] lg:w-fit lg:min-w-full lg:max-w-full">
-                <Image
-                  src={product.image}
-                  alt={`image of ${product.name}`}
-                  width={500}
-                  height={500}
-                  priority
-                  className="size-full rounded-[--card-radius] border object-cover group-hover:scale-105"
-                />
-              </div>
-            </div>
-
-            <div className="lg:pt-12">
-              <div className="mx-auto max-w-sm">
-                <div>
-                  <Title size="2xl" className="mb-3">
-                    {product.name}
-                  </Title>
-                  <Rating rating={4} ratings={456} />
-                  <Text size="base" className="mb-0 mt-6">
-                    ${product.price}
-                  </Text>
-                </div>
-
-                <form className="my-8 space-y-6">
-                  <SizeRadioGroup />
-                  <Button.Root className="w-full" size="lg" intent="neutral">
-                    <Button.Label>Add to Bag</Button.Label>
-                  </Button.Root>
-                </form>
-
-                <div>
-                  <Text>{product.shortDescription}</Text>
-
-                  <List>
-                    <li>List item</li>
-                    <li>List item</li>
-                    <li>List item</li>
-                    <li>List item</li>
-                    <li>List item</li>
-                  </List>
-                </div>
-
-                <RatingComparison
-                  rating={{
-                    one: 0,
-                    two: 0,
-                    three: 0,
-                    four: 5,
-                    five: 95,
-                  }}
-                />
-              </div>
-            </div>
-          </div>
+          <Suspense fallback={<ProductSkeleton />}>
+            <Product productName={params.name} />
+          </Suspense>
         </div>
       </section>
 
       <section className="pb-40 pt-24 sm:pt-32">
         <div className="mx-auto max-w-7xl px-6">
-          <Title as="h2" size="2xl">
+          <Title as="h2" size="2xl" className="mb-12">
             Similar Products
           </Title>
-          <div className="mt-12 grid gap-x-6 gap-y-12 sm:grid-cols-2 lg:grid-cols-3">
-            {products.slice(1, 4).map((product, index) => (
-              <Fragment key={index}>
-                <ProductCard {...product} />
-              </Fragment>
-            ))}
-          </div>
+          <Suspense fallback={<ProductListSkeleton items={3} />}>
+            <SimilarProducts />
+          </Suspense>
         </div>
       </section>
     </>
