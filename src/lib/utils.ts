ae24630ed4931c39c3024ee7c4eb5026f431b490
Théo Balick <balickmethens@gmail.com>
2024-08-03 11:48:46 +0100

    [feat] Add cn function to merge classes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,15 +1,13 @@
-import {twMerge} from "tailwind-merge";
+import { twMerge } from "tailwind-merge";
 import React from "react";
+import clsx, { ClassValue } from "clsx";
 
-/**
- * Clone React element.
- * The function clones React element and adds Tailwind CSS classnames to the cloned element
- * @param element the React element to clone
- * @param classNames Tailwind CSS classnames
- * @returns { React.ReactElement } - Cloned React element
- */
+export function cn(...inputs: ClassValue[]): string {
+  return twMerge(clsx(inputs));
+}
+
 export function cloneElement(element: React.ReactElement, classNames: string) {
   return React.cloneElement(element, {
-    className: twMerge(element.props.className, classNames)
+    className: twMerge(element.props.className, classNames),
   });
 }
